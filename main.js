05e0ce0fdcf5fd8b4d585ea9b21cfcf3b2fcdb03
Akinwole Adam <100495971+AkinwoleAdam@users.noreply.github.com>
2023-03-14 16:25:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let hamburger = document.querySelector('.hamburger');
+
+let navBar = document.querySelector('.nav-bar');
+
+hamburger.addEventListener('click', function(){
+  
+  navBar.classList.toggle('active');
+}
+
+);
